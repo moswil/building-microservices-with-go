086756e184948efc679e1cfbd0e7c0382a9e3c74
Moses Willfred <moseswillfred1@gmail.com>
2021-02-21 01:03:13 +0300

    created a more performant server and implemented graceful shutdown.

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/moswil/building-microservices-with-go/handlers"
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l := log.New(os.Stdout, "product-api: ", log.LstdFlags)
 	hh := handlers.NewHello(l)
 
 	sm := http.NewServeMux()
 	sm.HandleFunc("/", hh.ServeHTTP)
 
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
